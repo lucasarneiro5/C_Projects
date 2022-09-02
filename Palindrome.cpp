c4333e0a24bf133e32f10f6bdd48e6edd0b94e60
allan-drew <allanjfmg@hotmail.com>
2022-09-02 11:07:51 -0300

    changing some code in the palindrome project

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+
 // Define is_palindrome() here:
 bool is_palindrome(std::string text) {
   
@@ -17,8 +18,17 @@ bool is_palindrome(std::string text) {
   
 }
 
+
 int main() {
-  
+
+  std::cout << std::boolalpha; // bool as true and false (instead of 1 and 0 )
+  std::string typeTheWord;
+
+  std::cout << "Type the word to check if it is a palindrome : " << std::endl;
+  std::cin >> typeTheWord;
+  //std::cout << is_palindrome(typeTheWord) << std::endl;
+  std::cout << " The word " << typeTheWord << " is palindrome ? ---> " << is_palindrome(typeTheWord) << std::endl;
+
   std::cout << is_palindrome("madam") << "\n";
   std::cout << is_palindrome("ada") << "\n";
   std::cout << is_palindrome("lovelace") << "\n";
